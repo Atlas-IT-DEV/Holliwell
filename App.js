3594645e11a379e3fd08466810e95b6870837fcf
Sand Despotism <wowow3722123@gmail.com>
2024-06-23 23:05:07 +0300

    geologica added

--- App.js
+++ App.js
@@ -13,35 +13,44 @@ import IntroScreen2 from "./pages/login_screen/intro_screen2";
 import PrivacyPolicyScreen from "./pages/login_screen/privacy_policy_screen";
 import MainScreen from "./pages/login_screen/main_screen";
 import BurgerMenu from "./pages/login_screen/burger_menu";
+import { useFonts } from "expo-font";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    GeologicaMedium: require("./assets/fonts/Geologica-Medium.ttf"),
+    GeologicaRegular: require("./assets/fonts/Geologica-Regular.ttf"),
+    GeologicaLight: require("./assets/fonts/Geologica-Light.ttf"),
+    GeologicaThin: require("./assets/fonts/Geologica-Thin.ttf"),
+  });
   return (
     <NavigationContainer>
       <RootStoreContext.Provider value={new RootStore()}>
-        <Stack.Navigator
-          initialRouteName="LoginScreen"
-          screenOptions={{ headerShown: false }}
-        >
-          <Stack.Screen name="LoginScreen" component={LoginScreen} />
-          <Stack.Screen
-            name="RegistrationScreen"
-            component={RegistrationScreen}
-          />
-          <Stack.Screen
-            name="RecoveryPasswordScreen"
-            component={RecoveryPasswordScreen}
-          />
-          <Stack.Screen name="IntroScreen1" component={IntroScreen1} />
-          <Stack.Screen name="IntroScreen2" component={IntroScreen2} />
-          <Stack.Screen
-            name="PrivacyPolicyScreen"
-            component={PrivacyPolicyScreen}
-          />
-          <Stack.Screen name="MainScreen" component={MainScreen} />
-          <Stack.Screen name="BurgerMenu" component={BurgerMenu} />
-        </Stack.Navigator>
+        {fontsLoaded && (
+          <Stack.Navigator
+            initialRouteName="LoginScreen"
+            screenOptions={{ headerShown: false }}
+          >
+            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+            <Stack.Screen
+              name="RegistrationScreen"
+              component={RegistrationScreen}
+            />
+            <Stack.Screen
+              name="RecoveryPasswordScreen"
+              component={RecoveryPasswordScreen}
+            />
+            <Stack.Screen name="IntroScreen1" component={IntroScreen1} />
+            <Stack.Screen name="IntroScreen2" component={IntroScreen2} />
+            <Stack.Screen
+              name="PrivacyPolicyScreen"
+              component={PrivacyPolicyScreen}
+            />
+            <Stack.Screen name="MainScreen" component={MainScreen} />
+            <Stack.Screen name="BurgerMenu" component={BurgerMenu} />
+          </Stack.Navigator>
+        )}
       </RootStoreContext.Provider>
     </NavigationContainer>
   );
